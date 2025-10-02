75f52bc8f7af03a30d9709b26ff6b20bf9b4532b
JOSHUA FREZZELL <jafrezzell@msn.com>
2025-10-02 01:58:28 -0400

    Finished First Version of DADIT

--- Assets/Scripts/CharDeets.cs
+++ Assets/Scripts/CharDeets.cs
@@ -1,26 +1,63 @@
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CharDeets
-
+public class CharDeets : MonoBehaviour, IComparable<CharDeets>
 {
     public string Name { get { return _name; } set { SetName(value); } } // Name of the character
     public int Initiative { get { return _initative; } set { SetInitiative(value); } } // The current Intiative the character has
 
-    
-    public GameObject panelGameObject; // Panel gameobject that represents this entry panel
+    public int Health { get { return _health; } set { SetHealth(value); } }// Current health of the character.
+
+    public bool IsPlayer { get { return _isPlayer; } set { SetPlayerStatus(value); } }
+
+
+    TMP_Text _nameText;
+    TMP_InputField _initInputField;
+    TMP_InputField _healthInputField;
+    TMP_InputField _healthIncInputField;
+    TMP_InputField _healthDecInputField;
+    Button _upButton;
+    Button _downButton;
+    Button _removeCharacterButton;
+    Image _backgroundImage;
 
     int _initative = 0;
     string _name = "";
+    int _health = 1;
+    bool _isPlayer = false;
 
-    public CharDeets(string charName, int initiative, GameObject panelGameObject = null) 
+    private void Awake()
     {
-        this.panelGameObject = panelGameObject;
+        _nameText = transform.Find("TXT_Name").GetComponent<TMP_Text>();
+        _initInputField = transform.Find("IF_InitField").GetComponent<TMP_InputField>();
+        _healthInputField = transform.Find("IF_Health").GetComponent <TMP_InputField>();
+        _healthIncInputField = transform.Find("IF_HealthInc").GetComponent<TMP_InputField>();
+        _healthDecInputField = transform.Find("IF_HealthDec").GetComponent<TMP_InputField>();
+        _upButton = transform.Find("BTN_Up").GetComponent<Button>();
+        _downButton = transform.Find("BTN_Down").GetComponent<Button>();
+        _removeCharacterButton = transform.Find("BTN_Remove").GetComponent<Button>();
+        _backgroundImage = transform.GetComponent<Image>();
+
+
+        _upButton.onClick.AddListener(delegate { InitiativeBrain.Instance.ShiftCharacter(this, InitiativeBrain.Shift_Direction.UP); });
+        _downButton.onClick.AddListener(delegate { InitiativeBrain.Instance.ShiftCharacter(this, InitiativeBrain.Shift_Direction.DOWN); });
+        _removeCharacterButton.onClick.AddListener(delegate { InitiativeBrain.Instance.RemoveCharacterFromEncounter(this); });
+        _initInputField.onEndEdit.AddListener(delegate { Initiative = int.Parse(_initInputField.text); }); // WORK ON THIS
+        _healthInputField.onEndEdit.AddListener(delegate { Health = int.Parse(_healthInputField.text); });
+        _healthIncInputField.onEndEdit.AddListener(delegate { IncDecHealth(_healthIncInputField); });
+        _healthDecInputField.onEndEdit.AddListener(delegate { IncDecHealth(_healthDecInputField); });
+    }
+
+    public CharDeets(string charName, int initiative, int health = 1) 
+    {
         Name = charName;
         Initiative = initiative;
+        Health = health;
     }
 
     /// <summary>
@@ -29,12 +66,16 @@ public class CharDeets
     /// <param name="val">New initiative value</param>
     public void SetInitiative(int val)
     {
+        int oldInit = _initative;
         _initative = val;
 
-        if(panelGameObject != null)
+        if(gameObject != null)
         {
-            TMP_Text panelText = panelGameObject.transform.Find("TXT_description").GetComponent<TMP_Text>();
-            panelText.text = _name + ": " + _initative;
+            _initInputField.text = _initative.ToString();
+            if(InitiativeBrain.Instance.characterOrder.Contains(this))
+            {
+                InitiativeBrain.Instance.MoveCharacterOnInitChanged(this, _initative, oldInit);
+            }
         }
     }
 
@@ -45,11 +86,69 @@ public class CharDeets
     public void SetName(string val)
     {
         _name = val;
-
-        if(panelGameObject != null)
+        if (gameObject != null)
         {
-            TMP_Text panelText = panelGameObject.transform.Find("TXT_description").GetComponent<TMP_Text>();
-            panelText.text = _name + ": " + _initative;
+            _nameText.SetText(val);
         }
+    }
+
+    public void SetPlayerStatus(bool val)
+    {
+        _isPlayer = val;
+    }
+
+    public void SetBackgroundColor(Color color)
+    {
+        _backgroundImage.color = color;
+    }
+
+    public void SetHealth(int val)
+    {
+        _health = val;
+        if (gameObject != null)
+        {
+            _healthInputField.text = _health.ToString();
+        }
+    }
+
+    public void IncDecHealth(TMP_InputField inputField)
+    {
+        int val = 0;
+        if(int.TryParse(inputField.text, out val))
+        {
+            if (inputField == _healthDecInputField)
+                val = -val;
+            Health += val;
+        }
+
+        inputField.text = "";
+    }
+
+    /// <summary>
+    /// Compares this character deet based on Initiative
+    /// </summary>
+    /// <param name="other">Other character details</param>
+    /// <returns>0 if initiative is the same, 1 if this initiative is greater than other, -1 if other is greater</returns>
+    public int CompareTo(CharDeets other)
+    {
+        int val = 0;
+
+        if (_initative > other.Initiative)
+            val = -1;
+        else if(_initative < other.Initiative)
+            val = 1;
+
+        return val;
+    }
+
+    private void OnDestroy()
+    {
+        _upButton.onClick.RemoveAllListeners();
+        _downButton.onClick.RemoveAllListeners();
+        _healthInputField.onEndEdit.RemoveAllListeners();
+        _initInputField.onEndEdit.RemoveAllListeners();
+        _removeCharacterButton.onClick.RemoveAllListeners();
+        _healthDecInputField.onEndEdit.RemoveAllListeners();
+        _healthIncInputField.onEndEdit.RemoveAllListeners();
     }
 }
--- Assets/Scripts/InitiativeBrain.cs
+++ Assets/Scripts/InitiativeBrain.cs
@@ -1,26 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using Toggle = UnityEngine.UI.Toggle;
 
 public class InitiativeBrain : MonoBehaviour
 {
     public static InitiativeBrain Instance { get; private set; }
 
+    public enum Shift_Direction { UP, DOWN };
+
     //TODO:  GUI SHOULD HAVE DRAG AND DROP LIST OF CHARACTERS GOING
     //TODO:  HAVE TIE BREAKER TOGGLE THAT DETERMINES WHETHER ENEMIES OR PLAYERS GO FIRST IN A TIE
-    public SortedList<int, CharDeets> characterOrder = new SortedList<int, CharDeets>(); // Represents the order of characters in combat
-    public CharDeets currentChar; // Represents current character in combat
+    public List <CharDeets> characterOrder = new List<CharDeets>(); // Represents the order of characters in combat
+    public CharDeets currentChar { get; private set; } // Represents current character in combat
     public GameObject charEntryPanelPrefab; // Character Entry Panel Prefab
 
-
+    public int roundNumber { get; private set; }
+    public bool playerPriority { get; private set; } // Priority for if players go first in the instance of a tie, or enemies.
 
     ScrollRect _initScrollView;
-    TMP_InputField _newnameIF;
+    TMP_InputField _newNameIF;
     TMP_InputField _newInitiativeIF;
+    TMP_InputField _newHealthIF;
+
+    Button _nextCharButton;
+    Button _prevCharButton;
+
+
+    TMP_Text _roundNumberTXT;
+    TMP_Text _currentCharacterTXT;
+
+    Color _backgroundColor = new Color(255, 255, 255, 255);
 
     private void Awake()
     {
@@ -30,43 +47,125 @@ public class InitiativeBrain : MonoBehaviour
             return;
         }
         Instance = this;
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
         _initScrollView = GameObject.Find("InitiaveScrollView").GetComponent<ScrollRect>();
-        _newnameIF = GameObject.Find("IF_charName").GetComponent<TMP_InputField>();
+        _newNameIF = GameObject.Find("IF_charName").GetComponent<TMP_InputField>();
         _newInitiativeIF = GameObject.Find("IF_charInit").GetComponent<TMP_InputField>();
-    }
+        _newHealthIF = GameObject.Find("IF_charHealth").GetComponent<TMP_InputField>();
+        //_roundNumberTXT = GameObject.Find("TXT_RoundNum").GetComponent<TMP_Text>();
+        _currentCharacterTXT = GameObject.Find("TXT_CurrentCharacter").GetComponent<TMP_Text>();
+        _nextCharButton = GameObject.Find("BTN_NextCharacter").GetComponent<Button>();
+        _prevCharButton = GameObject.Find("BTN_PrevCharacter").GetComponent <Button>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _nextCharButton.onClick.AddListener(delegate { GoToNextCharacter(Shift_Direction.DOWN); });
+        _prevCharButton.onClick.AddListener(delegate { GoToNextCharacter(Shift_Direction.UP); });
     }
 
     /// <summary>
     /// Adds a character to the encounter
     /// </summary>
-    /// <param name="name">Name of character</param>
-    /// <param name="initiave">Initiave of newly added character</param>
     public void AddCharacterToEncounter()
     {
-
         int charInit;
+        int charHealth;
         if(!int.TryParse(_newInitiativeIF.text, out charInit))
         {
-            Debug.Log("Initiative entered was not a valid number.");
+            //Debug.Log("Initiative entered was not a valid number.");
             return;
         }
-        string charName = _newnameIF.text;
+        if(!int.TryParse(_newHealthIF.text, out charHealth))
+        {
+            //Debug.Log("Health entered was not a valid number.");
+            charHealth = 1;
+        }
+
+        string charName = _newNameIF.text;
 
         GameObject newPanelEntry = Instantiate(charEntryPanelPrefab);
-        newPanelEntry.transform.SetParent(_initScrollView.content.transform, false);
 
-        CharDeets newCharacter = new CharDeets(charName, charInit, newPanelEntry);
-        characterOrder.Add(charInit, newCharacter);
+        CharDeets newCharacter = newPanelEntry.GetComponent<CharDeets>();
+        newCharacter.Name = charName;
+        newCharacter.Initiative = charInit;
+        newCharacter.Health = charHealth;
+
+        // If there are no characters in the list, then just add the character.
+        if(characterOrder.Count < 1) 
+        {
+            characterOrder.Add(newCharacter);
+            newPanelEntry.transform.SetParent(_initScrollView.content.transform, false);
+        }
+        else
+        {
+            int i;
+
+            // Find the right index to add the new character.
+            for(i = 0; i < characterOrder.Count; i++)
+            {
+                if (newCharacter.Initiative < characterOrder[i].Initiative)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            characterOrder.Insert(i, newCharacter);
+            newPanelEntry.transform.SetParent(_initScrollView.content.transform, false);
+            newPanelEntry.transform.SetSiblingIndex(i);
+        }
+
+        SetCurrentCharacter(characterOrder[0]);
+
+        _newInitiativeIF.text = "";
+        _newHealthIF.text = "";
+        _newNameIF.text = "";
+    }
+
+    /// <summary>
+    /// Shifts the current character in the character order based on the direction
+    /// </summary>
+    /// <param name="dir">Direction for next character to be picked</param>
+    public void GoToNextCharacter(Shift_Direction dir)
+    {
+        int nextIndex = characterOrder.IndexOf(currentChar);
+
+        if(nextIndex < 0)
+        {
+            return;
+        }
+
+        nextIndex = dir == Shift_Direction.UP ? nextIndex - 1 : nextIndex + 1;
+
+        if(nextIndex < 0 || nextIndex > characterOrder.Count - 1)
+        {
+            nextIndex = dir == Shift_Direction.UP ? characterOrder.Count - 1 : 0;
+        }
+
+        SetCurrentCharacter(characterOrder[nextIndex]);
+    }
+
+    public void SetCurrentCharacter(CharDeets nextCharacter)
+    {
+        String nextCharacterText = "";
+
+        if(currentChar != null && characterOrder.Count > 1)
+        {
+            currentChar.SetBackgroundColor(_backgroundColor);
+        }
+        if(nextCharacter != null && nextCharacter != currentChar)
+        {
+            nextCharacter.SetBackgroundColor(Color.yellow);
+            nextCharacterText = nextCharacter.Name;
+        }
+        currentChar = nextCharacter;
+        _currentCharacterTXT.text = "Current Character: " + nextCharacterText;
     }
 
     /// <summary>
@@ -75,6 +174,148 @@ public class InitiativeBrain : MonoBehaviour
     /// <param name="character">Character to be removed</param>
     public void RemoveCharacterFromEncounter(CharDeets character)
     {
-        characterOrder.Remove(character.Initiative);
+        // If current character is the character to be removed
+        if (currentChar == character && characterOrder.Count > 1)
+        {
+            int currentCharIndex = characterOrder.IndexOf(character);
+
+            if(currentCharIndex == characterOrder.Count - 1)
+            {
+                SetCurrentCharacter(characterOrder[0]);
+            }
+            else
+            {
+                SetCurrentCharacter(characterOrder[currentCharIndex + 1]);
+            }
+        }
+        characterOrder.Remove(character);
+        Destroy(character.gameObject);
+    }
+
+    /// <summary>
+    /// Moves character based on the inputted Initiative
+    /// </summary>
+    /// <param name="character">Character to move</param>
+    /// <param name="initNum">New initiative</param>
+    /// <param name="oldInit">Old initiative</param>
+    public void MoveCharacterOnInitChanged(CharDeets character, int initNum, int oldInit)
+    {
+        if(characterOrder.Count < 2)
+        {
+            return;
+        }
+
+        int origIndex = characterOrder.IndexOf(character);
+        int i = -1;
+
+        // Find appropriate spot to switch Initiative 
+        if(initNum > oldInit)
+        {
+            for(i = origIndex - 1; i < characterOrder.Count; i--)
+            {
+                if(initNum < characterOrder[i].Initiative)
+                {
+                    i++;
+                    //charToSwitch = characterOrder[i - 1];
+                    break;
+                }
+            }
+        }
+        else if(initNum < oldInit)
+        {
+            for(i = origIndex + 1; i >= 0; i++)
+            {
+                if(initNum > characterOrder[i].Initiative)
+                {
+                    i--;
+                    //charToSwitch = characterOrder[i + 1];
+                    break;
+                }
+            }
+        }
+
+        Debug.Log("Moving to character spot: " + i + " " + characterOrder[i].Name);
+
+        if (i < 0) { return; }
+
+        Debug.Log("Moving to character spot: " + i + " " + characterOrder[i].Name);
+
+        characterOrder.Remove(character);
+        characterOrder.Insert(i, character);
+
+        character.transform.SetSiblingIndex(i);
+    }
+
+    /// <summary>
+    /// Clears all characters from the encounter
+    /// </summary>
+    public void ClearAllCharacters()
+    {
+        while(characterOrder.Count > 0)
+        {
+            RemoveCharacterFromEncounter(characterOrder.First());
+        }
+        currentChar = null;
+        _currentCharacterTXT.text = "Current Character: ";
+    }
+
+    public void SetPlayerPriority(bool val)
+    {
+        playerPriority = val;
+    }
+
+    /// <summary>
+    /// Sorts character order list and gameobjects
+    /// </summary>
+    public void SortCharacterOrderList()
+    {
+        characterOrder.Sort();
+        for(int i = 0; i < characterOrder.Count; i++)
+        {
+            characterOrder[i].transform.SetSiblingIndex(i);
+        }
+    }
+
+    /// <summary>
+    /// Shifts a characters order in the list by one based one the direction inputted.
+    /// </summary>
+    /// <param name="character">Character to be initially switched.</param>
+    /// <param name="dir">Direction to move them.</param>
+    public void ShiftCharacter(CharDeets character, Shift_Direction dir)
+    {
+        if(characterOrder.Count > 1)
+        {
+            CharDeets otherChar = null;
+            int ndxOfChar = characterOrder.IndexOf(character);
+            int otherCharNdx = -1;
+
+            if(dir == Shift_Direction.UP && ndxOfChar > 0)
+            {
+                otherCharNdx = ndxOfChar - 1;
+            }
+            else if(dir == Shift_Direction.DOWN && ndxOfChar < characterOrder.Count - 1)
+            {
+                otherCharNdx = ndxOfChar + 1;
+            }
+
+            if(otherCharNdx < 0)
+            {
+                return;
+            }
+
+            otherChar = characterOrder[otherCharNdx];
+
+            characterOrder[ndxOfChar] = otherChar;
+            characterOrder[otherCharNdx] = character;
+
+            character.transform.SetSiblingIndex(otherCharNdx);
+            otherChar.transform.SetSiblingIndex(ndxOfChar);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _nextCharButton.onClick.RemoveAllListeners();
+        _prevCharButton.onClick.RemoveAllListeners();
     }
 }
